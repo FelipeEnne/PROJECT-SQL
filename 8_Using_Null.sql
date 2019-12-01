d19eab4b704d98b7391e1d24e1e026a26a547a91
Felipe Enne <felipeenne@gmail.com>
2019-11-30 21:13:58 -0300

    Self Join

--- 8_Using_Null.sql
+++ 8_Using_Null.sql
@@ -79,10 +79,6 @@ GROUP BY dept.name
 
 
 
-
-
-
-
 -- 9)
 -- List the films where 'Harrison Ford' has appeared - but not in the starring role. 
 -- [Note: the ord field of casting gives the position of the actor. 
